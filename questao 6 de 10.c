1c038b2966bed278404ff36af75b255761128a96
Thalles16 <168739769+Thalles16@users.noreply.github.com>
2024-08-19 10:49:45 -0300

    Add files via upload

---
+++ questao 6 de 10.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+
+main () {
+	
+float raio;
+
+
+	printf("escreva o valor do raio do circulo");
+		scanf("%f",& raio);
+
+float area=3.14*(raio*raio);
+
+printf("a area do circulo e %f",area);
+
+
+
+}
