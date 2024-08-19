d4c38d992990dc7a1171848de91d10c06916d11f
Thalles16 <168739769+Thalles16@users.noreply.github.com>
2024-08-19 09:23:23 -0300

    Add files via upload

---
+++ questao 2 de 10.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+
+
+main () {
+	
+float numero1;
+float numero2;
+
+	printf("escreva um numero");
+		scanf("%f",& numero1);
+
+	printf("escreva um outro numero");
+		scanf("%f",& numero2);
+
+float soma=numero1+numero2;
+
+printf("a soma desses numeros e %f",soma);
+
+	
+return 0;
+}
