7fabc8c2e17777d35b2a1f9814c6868cee6e891e
Thalles16 <168739769+Thalles16@users.noreply.github.com>
2024-08-19 10:17:58 -0300

    Add files via upload

---
+++ questao 3 de 10.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+int main() {
+	
+int numero1;
+
+
+	printf("escreva um numero");
+		scanf("%i", &numero1);
+
+	if (numero1 % 2 == 0){
+	
+		printf("numero e par");
+	}
+	else{
+	
+		printf("numero impar");
+	}
+}
