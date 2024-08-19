f0a5e03a397538d061d000b3ac8c66540c7807d4
Thalles16 <168739769+Thalles16@users.noreply.github.com>
2024-08-19 10:38:45 -0300

    Add files via upload

---
+++ questao 5 de 10.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+
+
+main () {
+	
+float numero1;
+float numero2;
+float numero3;
+
+	printf("escreva um numero");
+		scanf("%f",& numero1);
+
+	printf("escreva um outro numero ");
+		scanf("%f",& numero2);
+		
+	printf("escreva um outro numero ");
+		scanf("%f",& numero3);
+
+float media=numero1+numero2+numero3/3;
+
+	printf("a media aritimedica desses numeros e %f",media);
+
+return 0;
+	
+}
