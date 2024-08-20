07fca5ec279f25222e5b54547f86a36914161560
Thalles <168739769+Thalles16@users.noreply.github.com>
2024-08-20 08:57:12 -0300

    Add files via upload

---
+++ questao 7 de 10.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+
+main () {
+	
+float celsius;
+
+
+	printf("escreva quantos graus esta em sua cidade ");
+		scanf("%f",& celsius);
+
+	
+float farenheit= celsius*1.8+32;	
+	
+printf("a temperatura da sua cidade em farenheit e %f",farenheit);
+
+	
+return 0;
+}
